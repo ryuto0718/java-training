568cf5bc9350391abae1b36ab097880c504b8d14
yasuda <ryuto-yasuda@infinityloop.co.jp>
2025-04-23 15:55:15 +0900

    練習3-3

---
+++ 練習3-3/練習3-3.java
@@ -0,0 +1,14 @@
+public class Main{
+    public static void main(String[] args){
+        int isHungry = 0;
+        String food = "イチゴ";
+        System.out.println("こんにちは");
+        if(isHungry == 0){
+            System.out.println("お腹がいっぱいです");
+        }else{
+            System.out.println("はらぺこです");
+        System.out.println(food + "をいただきます");
+        System.out.println("ごちそうさまでした");
+        }
+    }
+}
