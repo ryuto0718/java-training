2cb19e8496ba1b5a84f754f4b55ac97f6803a5e0
yasuda <ryuto-yasuda@infinityloop.co.jp>
2025-04-23 16:55:53 +0900

    大小

---
+++ ex-1-7/大小.java
@@ -0,0 +1,16 @@
+public class Main{
+    public static void main(String[] args){
+        int x = 10;
+        int y = 5;
+        System.out.println("x=" + x + ",y=" + y);
+        if(x > y){
+            System.out.println("xはyよりも大きい");
+        }
+        if(x == y){
+            System.out.println("xとyは等しい");
+        }
+        if(x < y){
+            System.out.println("xはyよりも小さい");
+        }
+    }
+}
