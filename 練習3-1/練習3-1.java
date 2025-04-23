3cfa1fe00e2dbc6e7306733ee8f03311739f6b63
yasuda <ryuto-yasuda@infinityloop.co.jp>
2025-04-23 15:53:50 +0900

    練習3-1

---
+++ 練習3-1/練習3-1.java
@@ -0,0 +1,20 @@
+public class Main{
+    public static void main(String[] args){
+        int weight = 60;
+        int age1 = 15;
+        int age2 = 16;
+        int age = 3;
+        String name = ("湊");
+        if(weight == 60) {
+            System.out.println(true);
+        }
+        if((age1 + age2) * 2 > 60){
+            System.out.println(true);
+        }
+        if(age / 2 == 1){
+            System.out.println(true);
+        }if(name == "湊"){
+            System.out.println(true);
+        }
+    }
+}
