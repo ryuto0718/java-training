53b7bc03b78824e00bea499ed75ccecdd0b21e32
yasuda <ryuto-yasuda@infinityloop.co.jp>
2025-04-23 15:55:53 +0900

    練習3-4

---
+++ 練習3-4/練習3-4.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main(String[] args){
+        boolean tenki = true;
+        if(tenki == false){
+            System.out.println("洗濯をします");
+            System.out.println("散歩にいきます");
+        }else{
+            System.out.println("映画を見ます");
+            System.out.println("寝ます");
+        }
+    }
+}
