a0a0be467af22d296b90f31a19a08393612e02f6
yasuda <ryuto-yasuda@infinityloop.co.jp>
2025-04-23 10:27:20 +0900

    1-1 Main.java

---
+++ 1-1/Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args){
+        System.out.println("RPG:すっきり魔王征伐");
+        System.out.println("Ver.0.1 by 湊");
+        System.out.println("＜ただいま鋭意学習・製作中＞");
+        System.out.println("プログラムを終了します");
+    }
+}
