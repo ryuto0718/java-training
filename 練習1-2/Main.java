8599104e11a8f7fe2bfe7a05de8622ad6cb43cfe
yasuda <ryuto-yasuda@infinityloop.co.jp>
2025-04-23 14:09:36 +0900

    練習1-2

---
+++ 練習1-2/Main.java
@@ -0,0 +1,14 @@
+public class Main{
+    public static void main(String[] args){
+        int a,b,c;
+        a = 3;
+        b = 5;
+        c = a * b;
+        System.out.print("縦幅");
+        System.out.print(a);
+        System.out.print("横幅");
+        System.out.print(b);
+        System.out.print("の長方形は、");
+        System.out.print(c);
+    }
+}
