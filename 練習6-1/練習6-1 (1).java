80b328b208757ad90e426579848714be5034be8f
yasuda <ryuto-yasuda@infinityloop.co.jp>
2025-04-23 17:50:24 +0900

    練習6-1

---
+++ 練習6-1/練習6-1 (1).java
@@ -0,0 +1,12 @@
+package comment.Kou;
+
+public class Kouhan {
+    public static void dcallDeae(){
+        System.out.println("えぇい、こしゃくな。くせ者だ！であえい！");
+    }
+    public static void showMondokro() throws Exception{
+        System.out.println("飛車さん、角さん。もういいでしょう。");
+        System.out.println("この紋所が目にはいらぬか！");
+        doTogame();
+    }
+}
---
+++ 練習6-1/練習6-1 (2).java
@@ -0,0 +1,9 @@
+import comment.Zen.Zenhan;
+public class Main{
+    public static void main(String[] args){
+        doWarusa();
+        doTogame();
+        callDeae();
+        showMondokoro();
+    }
+}
