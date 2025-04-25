ecac73f6fb54f40349975a0a5667885dbdd2a025
yasuda <ryuto-yasuda@infinityloop.co.jp>
2025-04-25 09:14:11 +0900

    8-2 Main.java

---
+++ 8-2/code8-2 p291.java
@@ -0,0 +1,9 @@
+public class Hero{
+    String name;
+    int hp;
+    public void attack() {...}
+    public void sleep() {...}
+    public void sit() {...}
+    public void slip() {...}
+    public void run() {...}
+}
