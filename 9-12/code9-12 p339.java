7d377be034fe740ba0d2713e908127b8bde792ae
yasuda <ryuto-yasuda@infinityloop.co.jp>
2025-05-07 09:48:47 +0900

    9-12 Main.java

---
+++ 9-12/code9-12 p339.java
@@ -0,0 +1,18 @@
+//次にHeroクラスを定義する
+public class Hero{
+    String name;  
+    int hp;      
+    Sword sword;        //勇者が装備している剣の情報
+    public void attack(){
+        System.out.println(this.name + "は攻撃した！");
+        System.out.println("敵に５ポイントのダメージをあたえた！");
+    }
+    public Hero(String name){
+        this.hp = 100;      //hpフィールドを100で初期化
+        this.name = name;       //引数の値でnameフィールドを初期化
+    }
+    public Hero(){
+        this.hp = 100;
+        this.name = "ダミー";
+    }
+}
