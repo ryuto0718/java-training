76f81d1c210f849a741a1901662f83721ec3dd4f
yasuda <ryuto-yasuda@infinityloop.co.jp>
2025-05-08 17:13:46 +0900

    11-10

---
+++ 11-10/無題.java
@@ -0,0 +1,16 @@
+//子クラス
+public class Computer extends TangibleAsset{
+    String makerName;
+    //コンストラクタ
+    public Computer(String name,int price,String color,String makerName){
+        this.name = name;
+        this.price = price;
+        this.color = color;
+        this.makerName = makerName;
+    }
+    //メソッド
+    public String getName(){return this.name;}
+    public int getPrice(){return this.price;}
+    public String getColor(){return this.color;}
+    public String getMakerName(){return this.makerName;}
+}
