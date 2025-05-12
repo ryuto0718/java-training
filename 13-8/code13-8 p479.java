890556be4e86601efdf6bdf84dbb64cac0957401
yasuda <ryuto-yasuda@infinityloop.co.jp>
2025-05-12 14:09:30 +0900

    13-8

---
+++ 13-8/code13-8 p479.java
@@ -0,0 +1,10 @@
+public class Hero{
+    private int hp;
+    String name;
+    Sword sword;
+    public String getName(){
+        return this.name;
+    }
+    public void bye(){
+        System.out.println("勇者は別れを告げた");
+    }
