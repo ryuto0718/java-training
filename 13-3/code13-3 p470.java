13408165af90e3ab5888411736b980c94f730d79
yasuda <ryuto-yasuda@infinityloop.co.jp>
2025-05-12 14:04:50 +0900

    13-3

---
+++ 13-3/code13-3 p470.java
@@ -0,0 +1,7 @@
+public class King {
+    public void talk(Hero h){
+        System.out.println("王様：ようこそ我が国へ、勇者" + h.name + "よ。");
+        System.out.println("王様：長旅疲れたであろう。");
+        h.die();
+    }
+}
