de5ad6527f71bdba612d370f026a1e46052c99b4
yasuda <ryuto-yasuda@infinityloop.co.jp>
2025-05-13 11:33:46 +0900

    17-7

---
+++ 17-7/code17-7 p640.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args){
+        FileWriter fw;      //mainメソッドブロック（5~13行目）はfwのスコープのまま
+        try{
+            fw = new FileWriter("data.txt");            //tryブロック内でコンストラクタが動作する
+            fw.write("hello!");         
+        }catch(Exception e){            
+            System.out.println("何らかの例外が発生しました");
+        }finally{
+        fw.close();         
+        }
+    }
+}
